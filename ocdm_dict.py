6b148d81cbb9d738ae4e372fed7777d78bdc779a
Cristel Chandre <cristel.chandre@cnrs.fr>
2022-02-13 11:54:50 +0100

    Minor edits

--- ocdm_dict.py
+++ ocdm_dict.py
@@ -5,7 +5,8 @@
 import numpy as xp
 
 
-Method = 'plot_potentials'
+Method = 'display_potentials'
+r_display = [3, 15]
 
 E0 = 2e-3
 omega = 2e-5
@@ -21,6 +22,7 @@ darkmode = True
 ###################################################################################################
 dict = {
         'Method': Method,
+		'r_display': xp.linspace(r_display[0], r_display[1], dpi),
         'E0': E0,
         'omega': omega,
         'SaveData': SaveData,
--- ocdm_modules.py
+++ ocdm_modules.py
@@ -39,16 +39,18 @@ def run_method(case):
     print('\033[92m    E0 = {:.2f}   omega = {:.2f}  \033[00m'.format(case.E0, case.omega))
     filestr = type(case).__name__ + '_' + 'E0{:.2f}_OM{:.2f}'.format(case.E0, case.omega).replace('.', '')
 
-    if case.Method == 'plot_potentials':
+    if case.Method == 'display_potentials':
         plt.rcParams.update({'figure.figsize': [16, 8]})
         fig, axs = plt.subplots(1, 2)
         axs[0].set_xlabel(r'$r$')
         axs[1].set_xlabel(r'$r$')
         axs[0].set_ylabel(r'$\varepsilon$')
-        r = xp.linspace(2, 15, 100)
-        axs[0].plot(r, case.eps(r), cs[1], lw=3)
-        axs[1].plot(r, case.al_para(r), cs[2], lw=3)
-        axs[1].plot(r, case.al_perp(r), cs[3], lw=3)
+		r = case.r_display 
+        axs[0].plot(r, case.eps(r), cs[1], lw=3, label=r'$\varepsilon(r)$')
+        axs[1].plot(r, case.al_para(r), cs[2], lw=3, label=r'$\alpha_\parallel(r)$')
+        axs[1].plot(r, case.al_perp(r), cs[3], lw=3, label=r'$\alpha_\perp(r)$')
+		axs[0].legend(loc='upper right', labelcolor='linecolor')
+		axs[1].legend(loc='upper right', labelcolor='linecolor')
         if case.SaveData:
             fig.savefig(filestr + '.png', dpi=case.dpi)
             print('\033[90m        Figure saved in {}.png \033[00m'.format(filestr))
