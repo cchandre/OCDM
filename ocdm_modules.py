170617af8a919c7b5ca31bc420773072e2df4602
Cristel Chandre <cchandre@gmail.com>
2022-03-10 12:14:43 +0100

    Update ocdm_modules.py

--- ocdm_modules.py
+++ ocdm_modules.py
@@ -1,4 +1,9 @@
 #
+# BSD 2-Clause License
+#
+# Copyright (c) 2021, Cristel Chandre
+# All rights reserved.
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
