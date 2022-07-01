85525365d24c2eebc11aebc41ffc5a50bc180e97
Cristel Chandre <cchandre@gmail.com>
2022-07-01 11:42:31 -0400

    Update ocdm.py
    - correction of a bug in check_dissociation

--- ocdm.py
+++ ocdm.py
@@ -181,7 +181,7 @@ class DiaMol:
 	def check_dissociation(self, y_):
 		if self.dim == 2:
 			dissociated = xp.zeros(len(y_) // 4, dtype=bool)
-			for _, (r, phi, p_r, p_phi) in enumerate(*zip(xp.split(y_, 4))):
+			for _, (r, phi, p_r, p_phi) in enumerate(zip(*xp.split(y_, 4))):
 				if xp.abs(p_phi) < self.p_phi_ion:
 					rs = root_scalar(lambda r_: self.d_eps(r_) * r_**3 - p_phi**2 / self.mu, bracket=[3.5, 5], xtol=1e-10, rtol=1e-10, method='brentq').root
 					Es = rs * self.d_eps(rs) / 2 + self.eps(rs)
